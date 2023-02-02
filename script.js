33aab9c6a31ef87b56a0e9df31fe924b8558646f
haizaki290 <124218190+haizaki290@users.noreply.github.com>
2023-02-02 08:38:20 +0800

    1 file modified

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('Hello!');
+const featuredGames = [
+  { name: "The Witcher 3: Wild Hunt", genre: "Action RPG" },
+  { name: "Red Dead Redemption 2", genre: "Action Adventure" },
+  { name: "Persona 5", genre: "JRPG" }
+];
+
+const upcomingGames = [
+  { name: "Elden Ring", genre: "Action RPG" },
+  { name: "Deathloop", genre: "FPS" },
+  { name: "Biomutant", genre: "Action RPG" }
+];
+
+const gameGenres = [
+  "Action RPG",
+  "Action Adventure",
+  "JRPG",
+  "FPS",
+  "Sports",
+  "Strategy"
+];
+
+const featuredGamesDiv = document.querySelector("#featured-games");
+const upcomingGamesDiv = document.querySelector("#upcoming-games");
+const
